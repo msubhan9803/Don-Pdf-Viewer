f4eaa6b1bf7647b979acb8ea0cf7455ccff0257a
Mohammad Subhan <mohammad.subhan@saylix.com>
2023-01-14 15:38:59 +0500

    in progress

--- web/script.js
+++ web/script.js
@@ -10,6 +10,7 @@ async function handleGetPdfSummarizedFile(event) {
     const file = document.getElementById('my-file-input').files[0];
 
     // Here I will hit DOn's api
+    const result = handleGetPdfSummary(file);
     summaryList = [
         "Since 2008, the role of awards has been revitalized due to the Rudd Labor government’s policy of ‘award modernization’, which has both restored the effectiveness of awards as a safety net for wages and working hours and expanded their coverage (Murray & Owens, 2009).",
         "This conceptual lacuna is particularly problematic because of the complexity of the processes under examination.",
@@ -46,13 +47,35 @@ async function handleGetPdfSummarizedFile(event) {
         });
 }
 
+function handleGetPdfSummary(file) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "multipart/form-data");
+    myHeaders.append("Access-Control-Allow-Origin", "*");
+    myHeaders.append("x_access_token", "xyJ3eXCiOiJKV2QiLCJhbGciOiXIUzI1NiX8");
+
+    var formdata = new FormData();
+    formdata.append("file", file, file.name);
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: formdata,
+        redirect: 'follow'
+    };
+
+    fetch("http://3.1.240.237/data_processing", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
+
 function initializeSummaryListCards(list) {
     const cardContainer = document.getElementById('card-container');
 
     for (let i = 0; i < list.length; i++) {
         const card = document.createElement('div');
         card.innerHTML = list[i];
-        card.onclick = function() {
+        card.onclick = function () {
             scrollToText(list[i]);
         }
         card.classList.add('card');
